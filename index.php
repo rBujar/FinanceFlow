4d0cd1296b9f6666ea8d90a739be7379823642f2
Bujar Roka <rokabujar@gmail.com>
2026-05-19 15:52:01 +0200

    finish setting it up

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require_once 'vite-helper.php'; ?>
+<!doctype html>
+<html lang="en">
+  <head>
+   <?php include 'components/head.php' ?>
+  </head>
+  <body>
+    <?php include 'components/header.php' ?>
+    <?php include 'components/footer.php' ?>
+    <?php viteEntry('src/js/main.js'); ?>
+  </body>
+</html>
