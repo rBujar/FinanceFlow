6e1ee825c36b965bc7ffac8124dd0ee590e276a3
Bujar Roka <rokabujar@gmail.com>
2026-05-27 21:54:46 +0200

    created and style explore section

--- index.php
+++ index.php
@@ -6,6 +6,22 @@
   </head>
   <body>
     <?php include 'components/header.php' ?>
+     <div class="explore-bg">
+    <section class="explore container">
+      <div class="row">
+        <div class="explore__content col-12 col-lg-6">
+          <div class="explore__title">
+            <h2>Explore endless possibilities with FinanceFlow</h2>
+            <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat nulla suspendisse tortor aene.</p>
+          </div>
+          <button class="white-btn">Download App</button>
+        </div>
+        <div class="explore__phone col-12 col-lg-6">
+          <img src="./src/assets/explore-phones.png" alt="token-phones">
+        </div>
+      </div>
+    </section>
+  </div>
     <?php include 'components/footer.php' ?>
     <?php viteEntry('src/js/main.js'); ?>
   </body>
