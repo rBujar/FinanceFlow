cac7d7ee19247f1e840b5ee6eb18346757916396
Bujar Roka <rokabujar@gmail.com>
2026-06-15 12:25:56 +0200

    created and styled blog post page

---
+++ blogPost.php
@@ -0,0 +1,152 @@
+<?php require_once 'vite-helper.php'; ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <?php include 'components/head.php' ?>
+</head>
+<body>
+    <?php include 'components/header.php' ?>
+    <div class="blog-bg">
+        <section class="blogPost container">
+            <div class="row">
+                <div class="blogPost__info col-12">
+                    <div class="blogPost__author">
+                        <img src="./src/assets/author.png" alt="author-img">
+                        <div class="blogPost__authorInfo">
+                            <h5 class="text-sm text-bold text-caps">Alex Turner</h5>
+                            <p class="text-bold text-caps">August 2, 2021</p>
+                        </div>
+                    </div>
+                    <div class="blogPost__title">
+                        <h2>The Basics about Cryptocurrency</h2>
+                        <p class="text-sm">Lorem ipsum dolor sit ametero irseo, consectetur adipiscing elit. Scelerisque viverra donec diammeo.</p>
+                    </div>
+                    <img src="./src/assets/post-image.png" alt="post-image">
+                </div>
+                <div class="blogPost__content col-12 col-md-10 offset-md-1">
+                    <div class="row">
+                        <div class="blogPost__item col-12 col-md-10 offset-md-1">
+                            <h2>Learn what you need to know before you invest in a virtual currency</h2>
+                            <p class="text-sm">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p class="text-sm">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div class="blogPost__item col-12 col-md-10 offset-md-1">
+                            <h2>Learn what you need to know before you invest in a virtual currency</h2>
+                            <p class="text-sm">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p class="text-sm">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <img src="./src/assets/post-image.png" alt="post-image col-12 col-md-10 col-lg-9">
+                        <div class="blogPost__item col-12 col-md-10 offset-md-1">
+                            <h2>Learn what you need to know before you invest in a virtual currency</h2>
+                            <p class="text-sm">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p class="text-sm">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div class="blogPost__card col-12">
+                            <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+                        </div>
+                        <div class="blogPost__item col-12 col-md-10 offset-md-1">
+                            <h2>Learn what you need to know before you invest in a virtual currency</h2>
+                            <p class="text-sm">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p class="text-sm">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <div class="blogSub-bg">
+        <section class="blogSub container">
+            <div class="row">
+                <div class="blogSub__title col-12 col-lg-6 col-xl-7">
+                    <img src="./src/assets/subscribe.png" alt="subscribe-image">
+                    <h2>Subscribe to our crypto news weekly newsletter!</h2>
+                </div>
+                <div class="blogSub__content col-12 col-lg-6 col-xl-5">
+                    <div class="blogSub__inputContainer">
+                        <input type="email" name="email" placeholder="Enter email address">
+                        <button class="blue-btn text-bold text-caps">Subscribe</button>
+                    </div>
+                    <button class="blogSub__darkBtn text-caps text-bold">Subscribe</button>
+                </div>
+            </div>
+        </section>
+    </div>
+    <div class="blog-bg">
+        <section class="post container">
+            <div class="row">
+                <div class="post__title col-12">
+                    <h2>Latest Post</h2>
+                    <button class="grey-btn selected">View All</button>
+                </div>
+                <div class="post__content col-12">
+                    <div class="post__item">
+                        <div class="post__image">
+                            <img src="./src/assets/blog-1.png" class="post__image" alt="blog-1">
+                        </div>
+                        <div class="post__info">
+                            <div class="post__articleType">
+                                <p class="text-caps">PRODUCTS</p>
+                            </div>
+                            <div class="post__infoTitle">
+                                <h4 class="text-lg text-bold">The Basics about Cryptocurrency</h4>
+                                <p class="text-sm">Lorem ipsum dolor sit ametero irseo, consectetur adipiscing elit. Scelerisque viverra donec diammeo.</p>
+                            </div>
+                            <div class="post__author">
+                                <img src="./src/assets/author.png" alt="author">
+                                <div class="post__authorInfo">
+                                    <h5 class="text-sm text-bold text-caps">Alex Turner</h5>
+                                    <span class="text-sm">AUGUST 2, 2021</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="post__item">
+                        <div class="post__image">
+                            <img src="./src/assets/blog-1.png" class="post__image" alt="blog-1">
+                        </div>
+                        <div class="post__info">
+                            <div class="post__articleType">
+                                <p class="text-caps">PRODUCTS</p>
+                            </div>
+                            <div class="post__infoTitle">
+                                <h4 class="text-lg text-bold">The Basics about Cryptocurrency</h4>
+                                <p class="text-sm">Lorem ipsum dolor sit ametero irseo, consectetur adipiscing elit. Scelerisque viverra donec diammeo.</p>
+                            </div>
+                            <div class="post__author">
+                                <img src="./src/assets/author.png" alt="author">
+                                <div class="post__authorInfo">
+                                    <h5 class="text-sm text-bold text-caps">Alex Turner</h5>
+                                    <span class="text-sm">AUGUST 2, 2021</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="post__item">
+                        <div class="post__image">
+                            <img src="./src/assets/blog-1.png" class="post__image" alt="blog-1">
+                        </div>
+                        <div class="post__info">
+                            <div class="post__articleType">
+                                <p class="text-caps">PRODUCTS</p>
+                            </div>
+                            <div class="post__infoTitle">
+                                <h4 class="text-lg text-bold">The Basics about Cryptocurrency</h4>
+                                <p class="text-sm">Lorem ipsum dolor sit ametero irseo, consectetur adipiscing elit. Scelerisque viverra donec diammeo.</p>
+                            </div>
+                            <div class="post__author">
+                                <img src="./src/assets/author.png" alt="author">
+                                <div class="post__authorInfo">
+                                    <h5 class="text-sm text-bold text-caps">Alex Turner</h5>
+                                    <span class="text-sm">AUGUST 2, 2021</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <?php include 'components/footer.php' ?>
+    <?php viteEntry('src/js/main.js'); ?>
+</body>
+</html>
