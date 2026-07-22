45c75f6ed18f864298e756ca8ce7e58abec2ce9d
Bujar Roka <rokabujar@gmail.com>
2026-07-22 17:09:09 +0200

    change the name of token section to rewards

--- index.php
+++ index.php
@@ -93,58 +93,58 @@
         </div>
       </section>
     </div>
-  <div class="token-bg">
-    <section class="token container">
+  <div class="rewards-bg">
+    <section class="rewards container">
       <div class="row">
-        <div class="token__card col-12">
+        <div class="rewards__card col-12">
           <div class="row">
-            <div class="token__image token__image--top col-8 offset-2 col-lg-5 offset-lg-0">
-              <img src="./src/assets/token-phones.png" alt="token phone">
+            <div class="rewards__image rewards__image--top col-8 offset-2 col-lg-5 offset-lg-0">
+              <img src="./src/assets/rewards-phones.png" alt="rewards phone">
             </div>
-            <div class="token__content col-12 col-lg-5 offset-lg-2 col-xl-4 offset-xl-3">
+            <div class="rewards__content col-12 col-lg-5 offset-lg-2 col-xl-4 offset-xl-3">
               <h2>Earn daily rewards on your idle tokens</h2>
               <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat nulla suspendisse
                 tortor aene.</p>
-              <div class="token__features">
-                <div class="token__item">
-                  <img src="./src/assets/token-1.png" alt="token-1">
+              <div class="rewards__features">
+                <div class="rewards__item">
+                  <img src="./src/assets/rewards-1.png" alt="rewards-1">
                   <p class="text-sm">Lowest fees in market</p>
                 </div>
-                <div class="token__item">
-                  <img src="./src/assets/token-2.png" alt="token-2">
+                <div class="rewards__item">
+                  <img src="./src/assets/rewards-2.png" alt="rewards-2">
                   <p class="text-sm">Fast and secure transactions</p>
                 </div>
-                <div class="token__item">
-                  <img src="./src/assets/token-3.png" alt="token-3">
+                <div class="rewards__item">
+                  <img src="./src/assets/rewards-3.png" alt="rewards-3">
                   <p class="text-sm">256-bit secure encryption</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="token__card col-12">
+        <div class="rewards__card col-12">
           <div class="row">
-            <div class="token__content col-12 col-lg-5 col-xl-4">
+            <div class="rewards__content col-12 col-lg-5 col-xl-4">
               <h2>Earn daily rewards on your idle tokens</h2>
               <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat nulla suspendisse
                 tortor aene.</p>
-              <div class="token__features">
-                <div class="token__item">
-                  <img src="./src/assets/token-4.png" alt="token-4">
+              <div class="rewards__features">
+                <div class="rewards__item">
+                  <img src="./src/assets/rewards-4.png" alt="rewards-4">
                   <p class="text-sm">100% Private data</p>
                 </div>
-                <div class="token__item">
-                  <img src="./src/assets/token-5.png" alt="token-5">
+                <div class="rewards__item">
+                  <img src="./src/assets/rewards-5.png" alt="rewards-5">
                   <p class="text-sm">99.99% Uptime guarantee</p>
                 </div>
-                <div class="token__item">
-                  <img src="./src/assets/token-6.png" alt="token-6">
+                <div class="rewards__item">
+                  <img src="./src/assets/rewards-6.png" alt="rewards-6">
                   <p class="text-sm">24/7 Dedicated support</p>
                 </div>
               </div>
             </div>
-            <div class="token__image token__image--bottom col-12 col-lg-7 col-xl-8">
-              <img src="./src/assets/token-laptop.png" alt="token phone">
+            <div class="rewards__image rewards__image--bottom col-12 col-lg-7 col-xl-8">
+              <img src="./src/assets/rewards-laptop.png" alt="rewards phone">
             </div>
           </div>
         </div>
@@ -162,7 +162,7 @@
           <button class="white-btn">Download App</button>
         </div>
         <div class="explore__phone col-12 col-lg-6">
-          <img src="./src/assets/explore-phones.png" alt="token-phones">
+          <img src="./src/assets/explore-phones.png" alt="rewards-phones">
         </div>
       </div>
     </section>
